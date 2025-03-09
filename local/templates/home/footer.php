0465e5e37ce03550a2900f47a698d8aa9536bf72
ALINA122355994 <alin.gaffarova@yandex.com>
2025-03-09 16:17:16 +0300

    183872

--- local/templates/home/footer.php
+++ local/templates/home/footer.php
@@ -4,10 +4,20 @@
       <div class="row">
         <div class="col-lg-4">
           <div class="mb-5">
-            <h3 class="footer-heading mb-4">About HomeSpace</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe pariatur reprehenderit vero atque,
-              consequatur id ratione, et non dignissimos culpa? Ut veritatis, quos illum totam quis blanditiis, minima
-              minus odio!</p>
+            
+            <?$APPLICATION->IncludeComponent(
+	             "bitrix:main.include", 
+	             ".default", 
+	             array(
+	             	"AREA_FILE_SHOW" => "file",
+	             	"AREA_FILE_SUFFIX" => "inc",
+	             	"EDIT_TEMPLATE" => "",
+	             	"COMPONENT_TEMPLATE" => ".default",
+	             	"PATH" => "/include/about_homespace.php"
+	             ),
+	             false
+             );?>
+           
           </div>
 
 
@@ -40,15 +50,16 @@
         </div>
 
         <div class="col-lg-4 mb-5 mb-lg-0">
-          <h3 class="footer-heading mb-4">Follow Us</h3>
-
-          <div>
-            <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
-            <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
-            <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
-            <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
-          </div>
-
+          
+        <?$APPLICATION->IncludeComponent("bitrix:main.include", "follow_us", Array(
+	"AREA_FILE_SHOW" => "file",	// Показывать включаемую область
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",	// Шаблон области по умолчанию
+		"COMPONENT_TEMPLATE" => ".default",
+		"PATH" => "/include/follow_us.php",	// Путь к файлу области
+	),
+	false
+);?>
 
 
         </div>
@@ -56,14 +67,15 @@
       </div>
       <div class="row pt-5 mt-5 text-center">
         <div class="col-md-12">
-          <p>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            Copyright &copy;</script>
-            <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with
-            <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com"
-              target="_blank">Colorlib</a>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-          </p>
+        <?$APPLICATION->IncludeComponent("bitrix:main.include", "follow_us", Array(
+	"AREA_FILE_SHOW" => "file",	// Показывать включаемую область
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",	// Шаблон области по умолчанию
+		"COMPONENT_TEMPLATE" => ".default",
+		"PATH" => "/include/date.php",	// Путь к файлу области
+	),
+	false
+);?>
         </div>
 
       </div>
--- local/templates/home/header.php
+++ local/templates/home/header.php
@@ -32,6 +32,7 @@ Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/css/style.css");
 </head>
 <body>
 <div><?$APPLICATION->ShowHead();?><div>
+<div id="panel"><?$APPLICATION->ShowPanel();?></div>
   <div class="site-loader"></div>
   <div class="site-wrap">
     <div class="site-mobile-menu">
@@ -48,15 +49,51 @@ Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/css/style.css");
           <div class="col-6 col-md-6">
             <p class="mb-0">
               <a href="#" class="mr-3"><span class="text-black fl-bigmug-line-phone351"></span> <span
-                  class="d-none d-md-inline-block ml-2">+2 102 3923 3922</span></a>
-              <a href="#"><span class="text-black fl-bigmug-line-email64"></span> <span
-                  class="d-none d-md-inline-block ml-2">info@domain.com</span></a>
+                  class="d-none d-md-inline-block ml-2">
+  <?$APPLICATION->IncludeComponent(
+	"bitrix:main.include", 
+	".default", 
+	array(
+		"AREA_FILE_SHOW" => "file",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",
+		"COMPONENT_TEMPLATE" => ".default",
+		"PATH" => "/include/phone.php"
+	),
+	false
+);?>
+</span></a>
+<a href="#"><span class="text-black fl-bigmug-line-email64"></span> 
+<span class="d-none d-md-inline-block ml-2">
+<?$APPLICATION->IncludeComponent(
+	"bitrix:main.include", 
+	".default", 
+	array(
+		"AREA_FILE_SHOW" => "file",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",
+		"COMPONENT_TEMPLATE" => ".default",
+		"PATH" => "/include/mail.php"
+	),
+	false
+);?>
+                  
+</span></a>
             </p>
           </div>
           <div class="col-6 col-md-6 text-right">
-            <a href="#" class="mr-3"><span class="text-black icon-facebook"></span></a>
-            <a href="#" class="mr-3"><span class="text-black icon-twitter"></span></a>
-            <a href="#" class="mr-0"><span class="text-black icon-linkedin"></span></a>
+<?$APPLICATION->IncludeComponent(
+	"bitrix:main.include", 
+	".default", 
+	array(
+		"AREA_FILE_SHOW" => "file",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",
+		"COMPONENT_TEMPLATE" => ".default",
+		"PATH" => "/include/messenger.php"
+	),
+	false
+);?>
           </div>
         </div>
       </div>
@@ -66,20 +103,56 @@ Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/css/style.css");
       <div class="container py-1">
         <div class="row align-items-center">
           <div class="col-8 col-md-8 col-lg-4">
-            <h1 class=""><a href="index.html" class="h5 text-uppercase text-black"><strong>HomeSpace<span
-                    class="text-danger">.</span></strong></a></h1>
-          </div>
+            <h1 class=""><a href="index.html" class="h5 text-uppercase text-black"><strong>
+               <?$APPLICATION->IncludeComponent(
+	             "bitrix:main.include", 
+	             ".default", 
+	             array(
+	             	"AREA_FILE_SHOW" => "file",
+	             	"AREA_FILE_SUFFIX" => "inc",
+	             	"EDIT_TEMPLATE" => "",
+	             	"COMPONENT_TEMPLATE" => ".default",
+	             	"PATH" => "/include/homespace.php"
+	             ),
+	             false
+             );?>
+<span class="text-danger">.</span></strong></a></h1>
+</div>
+
+
           <div class="col-4 col-md-4 col-lg-8">
             <nav class="site-navigation text-right text-md-right" role="navigation">
               <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#"
                   class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
-              <ul class="site-menu js-clone-nav d-none d-lg-block">
+                  <?$APPLICATION->IncludeComponent("bitrix:menu", "top_menu", Array(
+	"ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
+		"CHILD_MENU_TYPE" => "left",	// Тип меню для остальных уровней
+		"DELAY" => "N",	// Откладывать выполнение шаблона меню
+		"MAX_LEVEL" => "2",	// Уровень вложенности меню
+		"MENU_CACHE_GET_VARS" => array(	// Значимые переменные запроса
+			0 => "",
+		),
+		"MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
+		"MENU_CACHE_TYPE" => "A",	// Тип кеширования
+		"MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
+		"ROOT_MENU_TYPE" => "top",	// Тип меню для первого уровня
+		"USE_EXT" => "N",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
+	),
+	false
+);?>
+              
+              
+                  <ul class="site-menu js-clone-nav d-none d-lg-block">
+
+
                 <li class="active">
+                
                   <a href="index.html">Home</a>
                 </li>
                 <li class="has-children">
                   <a href="properties.html">Properties</a>
                   <ul class="dropdown">
+                
                     <li><a href="#">Buy</a></li>
                     <li><a href="#">Rent</a></li>
                     <li><a href="#">Lease</a></li>
